ff1b1e3b9c5a75db54b20b87b74e9da5842240f5
Tsoof <tsoof.haziza@post.runi.ac.il>
2024-12-18 00:35:18 +0200

    changes in peak detecting and more calculations

--- back_end/server.py
+++ back_end/server.py
@@ -6,7 +6,18 @@ from video_route import setup_video_route
 app = Flask(__name__)
 
 setup_video_route(app)
-# setup_status_routes(app)
+
+
+# Health check route
+@app.route('/', methods=['GET'])
+def index():
+    return "App is running!", 200
+
+
+@app.route('/health', methods=['GET'])
+def health():
+    return "OK", 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
