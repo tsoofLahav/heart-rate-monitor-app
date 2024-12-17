466083db2e8c89f9124cc4e1b3afe83ad35a1a3a
Tsoof <tsoof.haziza@post.runi.ac.il>
2024-12-18 00:25:29 +0200

    changes in peak detecting and more calculations

--- back_end/video_route.py
+++ back_end/video_route.py
@@ -7,7 +7,6 @@ from peak_detection import peaks_detection
 def setup_video_route(app):
     @app.route('/process_video', methods=['POST'])
     def process_video():
-
         try:
             # Receive video file from request
             file = request.files['video']
@@ -17,6 +16,9 @@ def setup_video_route(app):
             # Open video and read frames
             cap = cv2.VideoCapture(video_path)
             fps = cap.get(cv2.CAP_PROP_FPS)
+
+            if not cap.isOpened():
+                raise Exception("Failed to open video file.")
 
             intensities = []
             while cap.isOpened():
@@ -35,9 +37,17 @@ def setup_video_route(app):
                 peaks.append(1)
                 for i in range(len(peaks) - 1):
                     differences.append(peaks[i+1] - peaks[i])
+            else:
+                differences.append(-1)
 
-                return jsonify({'heart_rate': bpm, 'average_gap': hrv, 'peaks': differences, 'startNew': startnew})
+            return jsonify({'heart_rate': bpm, 'average_gap': hrv, 'peaks': differences, 'startNew': startnew})
 
         except Exception as e:
             print(f"Error processing signal: {e}")
-            return jsonify({'heart_rate': 0.0, 'peaks': []})
+            return jsonify({
+                'error': f'Error processing signal: {str(e)}',
+                'heart_rate': 0.0,
+                'peaks': [],
+                'startNew': False
+            }), 500
+
