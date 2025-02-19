2a65ea3b7791c2e691ff6a4606d2ebf07306d2c0
Tsoof <tsoof.haziza@post.runi.ac.il>
2025-02-19 23:35:24 +0200

    “meow”

--- back_end/process_data.py
+++ back_end/process_data.py
@@ -7,16 +7,16 @@ def detect_pulse(intensities, fps):
     #
     #
     # detect peaks and unstable reading using peak_detection file
-    signal = np.array(intensities)
-    filtered_signal = peak_detection.moving_average_filter(signal, window_size=3)
+    filtered_signal = np.array(intensities)
+    #filtered_signal = peak_detection.moving_average_filter(signal, window_size=2)
     baseline = np.mean(filtered_signal)
     std_dev = np.std(filtered_signal)
     not_reading = peak_detection.detect_unstable_reading(filtered_signal, baseline, std_dev)
     if not_reading:
         return True, [], False, 0.0
-    dynamic_threshold = baseline + (0.5 * std_dev)
+    dynamic_threshold = baseline + (0.2 * std_dev)
     peaks = peak_detection.detect_peaks(filtered_signal, dynamic_threshold)
-    total_duration = len(signal) / fps
+    total_duration = len(filtered_signal) / fps
     #
     #
     # create new data to send to front end, using the peaks, using create_data file
